895a689291c72b560c929ae8f9ea7b977d512ae4
Dietrich Ayala <autonome@gmail.com>
2014-09-15 12:44:47 -0700

    updated to new SDK UI stuff, made URL list configurable instead of app tabs.

--- data/panel.js
+++ data/panel.js
@@ -2,3 +2,12 @@
 var matches = window.document.querySelectorAll('a');
 for (var i = 0; i < matches.length; i++)
   matches[i].setAttribute('target', '_blank');
+
+window.addEventListener('click', function(event) {
+  self.port.emit('click-link')
+  /*
+  var t = event.target
+  if (t.nodeName == 'A')
+    self.port.emit('click-link', t.toString())
+  */
+}, false)

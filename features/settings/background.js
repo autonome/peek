6c49428d43adcfe6da8232b413f6fd270077b405
dietrich ayala <me@burrito.space>
2023-05-10 22:23:46 +0200

    get settings working

--- features/settings/background.js
+++ features/settings/background.js
@@ -1,67 +1,22 @@
-// settings/settings.js
+// settings/background.js
 (async () => {
 
-console.log('settings/settings');
+console.log('settings/background');
+
+//import { labels, schemas, ui, defaults } from './config.js';
 
 //const debug = window.location.search.indexOf('debug') > 0;
 const debug = 1;
 
 if (debug) {
   console.log('clearing storage')
-  localStorage.clear();
+  //localStorage.clear();
 }
 
-const labels = {
-  featureType: 'settings',
-  featureDisplay: 'Settings',
-  prefs: {
-    shortcutKey: 'Settings shortcut',
-  }
-};
+const _store = localStorage;
+const _api = window.app;
 
-let _store = null;
-let _data = {};
-
-const prefsSchema = {
-  "$schema": "https://json-schema.org/draft/2020-12/schema",
-  "$id": "peek.prefs.schema.json",
-  "title": "Application and Settings preferences",
-  "description": "Peek user preferences",
-  "type": "object",
-  "properties": {
-    "shortcutKey": {
-      "description": "App keyboard shortcut to load settings",
-      "type": "string",
-      "default": "CommandOrControl+,"
-    },
-    "height": {
-      "description": "User-set or -defined height of Settings page",
-      "type": "integer",
-      "default": 600
-    },
-    "width": {
-      "description": "User-set or -defined width of Settings page",
-      "type": "integer",
-      "default": 800
-    },
-  },
-  "required": [ "shortcutKey" ]
-};
-
-// TODO: schemaize 0-9 constraints for peeks
-const schemas = {
-  prefs: prefsSchema,
-};
-
-const _defaults = {
-  prefs: {
-    shortcutKey: 'Option+,',
-    height: 600,
-    width: 800,
-  },
-};
-
-const openSettingsWindow = (api, prefs) => {
+const openSettingsWindow = (prefs) => {
   const height = prefs.height || 600;
   const width = prefs.width || 800;
 
@@ -73,38 +28,32 @@ const openSettingsWindow = (api, prefs) => {
     width
   };
 
-  api.openWindow(params);
+  _api.openWindow(params);
 };
 
-const initStore = (store, data) => {
-  const sp = store.getItem('prefs');
+const initStore = (data) => {
+  const sp = _store.getItem('prefs');
   if (!sp) {
-    store.setItem('prefs', JSON.stringify(data.prefs));
+    console.log('first run, initing datastore')
+    _store.setItem('prefs', JSON.stringify(data.prefs));
   }
 };
 
-const initShortcut = (api, prefs) => {
-  const shortcut = prefs.shortcutKey;
-  api.shortcuts.register(shortcut, () => {
-    openSettingsWindow(api, prefs);
+const initShortcut = (shortcut) => {
+  console.log('is', prefs());
+  _api.shortcuts.register(shortcut, () => {
+    openSettingsWindow(prefs());
   });
 };
 
-const init = (api, store) => {
+const prefs = () => JSON.parse(_store.getItem('prefs'));
+
+const init = () => {
   console.log('settings: init');
 
-  _store = store;
+  initStore(defaults);
 
-  initStore(_store, _defaults);
-
-  _data = {
-    get prefs() { return JSON.parse(_store.getItem('prefs')); },
-    get items() { return JSON.parse(_store.getItem('items')); }
-  };
-
-  console.log('data', _data);
-
-  initShortcut(api, _data.prefs);
+  initShortcut(prefs().shortcutKey);
 };
 
 const onChange = (changed, old) => {
@@ -121,36 +70,7 @@ const onChange = (changed, old) => {
   }
 };
 
-// ui config
-const config = {
-  // allow user to create new items
-  allowNew: false,
-  // fields that are view only
-  disabled: ['keyNum'],
-};
 
-const open = () => {
-  openSettingsWindow(_api, _data.prefs);
-};
-
-/*
-module.exports = {
-  init: init,
-  config,
-  labels,
-  schemas,
-  data: {
-    get prefs() {
-      return _store.get('prefs');
-    }
-  },
-  open,
-  onChange
-};
-*/
-
-window.addEventListener('load', () => {
-  init(window.app, localStorage);
-});
+window.addEventListener('load', init);
 
 })();

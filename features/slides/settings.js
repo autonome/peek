090117c5ec90747bf37ec76e8ddba1bee52c89f5
dietrich ayala <me@burrito.space>
2024-04-03 19:33:42 +0200

    settings for all my friends

---
+++ features/slides/settings.js
@@ -0,0 +1,96 @@
+import { id, labels, schemas, storageKeys, defaults } from './config.js';
+import { log as l, openStore, addToGUI } from "../utils.js";
+import GUI from './../../node_modules/lil-gui/dist/lil-gui.esm.min.js';
+
+const log = function(...args) { l(id, args); };
+
+log('background', id);
+
+const debug = window.app.debug;
+const clear = false;
+
+const store = openStore(id, defaults, clear /* clear storage */);
+const api = window.app;
+
+const container = document.querySelector('.houseofpane');
+let prefs = store.get(storageKeys.PREFS);
+let items = store.get(storageKeys.ITEMS);
+
+const persistToStorage = () => {
+  store.set(storageKeys.PREFS, prefs);
+  store.set(storageKeys.ITEMS, items);
+};
+
+const init = () => {
+
+  /*
+    pubsub.publish('open', {
+      feature: msg.prefs.startupFeature
+    });
+  */
+
+  // Initialize settings UI
+  const container = document.querySelector('.houseofpane');
+
+  const gui = new GUI({
+    touchStyles: false,
+    container: container,
+    title: labels.featureDisplay
+  });
+
+  // anytime anything changes, persist to storage
+  gui.onFinishChange(persistToStorage);
+
+  // Add prefs
+  const prefsFolder = gui.addFolder(
+    schemas.prefs.title
+  );
+
+	const pProps = schemas.prefs.properties;
+  const data = prefs;
+
+  Object.keys(pProps).forEach(k => {
+    // schema for property
+    const s = pProps[k];
+
+    // value (or default)
+    const v =
+      (data && data.hasOwnProperty(k))
+      ? data[k]
+      : pProps[k].default;
+
+    const disabled = false;
+    const step = pProps[k].type == 'integer' ? 1 : null;
+
+    addToGUI(prefsFolder, k, v, disabled, step).onChange(e => {
+      // TODO: validate new value against schema
+      prefs[k] = e;
+    });;
+  });
+
+  // Add items
+  items.forEach((item, i) => {
+    const folder = gui.addFolder(item.title);
+
+    addToGUI(folder, 'Key mapping', item.keyNum).disable();
+    addToGUI(folder, 'Address to load', item.address).onChange(e => {
+      items[i].address = e;
+    });
+    addToGUI(folder, 'Persist state (not supported)', item.persistState).disable();
+    addToGUI(folder, 'Keep live', item.keepLive).onChange(e => {
+      items[i].keepLive = e;
+    });
+    addToGUI(folder, 'Allow sound (not supported)', item.allowSound).disable();
+    addToGUI(folder, 'Window height', item.height).onChange(e => {
+      items[i].height = e;
+    });
+    addToGUI(folder, 'Window width', item.width).onChange(e => {
+      items[i].width = e;
+    });
+    addToGUI(folder, 'Enabled', item.enabled).onChange(e => {
+      items[i].enabled = e;
+    });
+  });
+};
+
+window.addEventListener('load', init);
--- preload.js
+++ preload.js
@@ -154,3 +154,14 @@ const handleMainWindow = () => {
   });
 };
 */
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
